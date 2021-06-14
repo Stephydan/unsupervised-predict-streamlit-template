2c41a9a1f16f2bf86a42a50291093addb45b023c
Stephydan <68903246+Stephydan@users.noreply.github.com>
2021-06-14 14:58:17 +0200

    Update collaborative_based.py

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -32,7 +32,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import copy
-from surprise import Reader, Dataset
+from surprise import reader, dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
